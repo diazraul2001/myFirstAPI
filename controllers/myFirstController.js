52a1c3109a3f5f6f9ba448d3c01f978f3261a3e4
diazraul2001 <91148063+diazraul2001@users.noreply.github.com>
2021-10-05 19:10:35 +0200

    hey

--- controllers/myFirstController.js
+++ controllers/myFirstController.js
@@ -1,5 +1,6 @@
 module.exports.helloWorld = (req, res) => {
     const responseObj = {msg: 'Hello World!'};
+    const nomAlumnos = ['Alex','Gerard','Roger'];
     const responseStatus = 200;
-res.status(responseStatus).send(responseObj);
+res.status(responseStatus).send(nomAlumnos);
 };
