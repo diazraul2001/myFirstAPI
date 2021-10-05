899fa562e192a99941a6089f57e17515de1c1f8a
diazraul2001 <91148063+diazraul2001@users.noreply.github.com>
2021-10-05 19:15:03 +0200

    y

--- controllers/myFirstController.js
+++ controllers/myFirstController.js
@@ -2,7 +2,7 @@ module.exports.helloWorld = (req, res) => {
     let responseObj = {};
     const nomAlumnos = ['Alex','Gerard','Roger'];
     responseObj.alumnos = nomAlumnos;
-    responseObj.countAulumnos = 3;
+    responseObj.countAulumnos = nomAlumnos.length;
     const responseStatus = 200;
 res.status(responseStatus).send(responseObj);
 };
