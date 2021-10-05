b01e322ea78f2793febbfb7249da36c92803226f
diazraul2001 <91148063+diazraul2001@users.noreply.github.com>
2021-10-05 18:59:06 +0200

    g

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const myFirstController = require('./controllers/myFirstController');
 
 app.get('/', myFirstController.helloWorld);
 
-app.listen(3000, () => {
+app.listen((process.env.PORT || 3000, function() {
     console.log('My first API running!');
 })
