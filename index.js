95680707e6e792d45a2f6a3baa96ffedcd6ca209
diazraul2001 <91148063+diazraul2001@users.noreply.github.com>
2021-10-19 19:27:54 +0200

    a

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const myFirstController = require('./controllers/myFirstController');
 
 app.get('/', myFirstController.helloWorld);
 
-app.use('api/v1/user', require('./routes/userRoutes.js'));
+app.use('/api/v1/user', require('./routes/userRoutes.js'));
 
 app.listen((process.env.PORT || 3000), function() {
     console.log('My first API running!');
