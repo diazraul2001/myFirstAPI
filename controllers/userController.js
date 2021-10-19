2d01ad916188d5b8c061284c898a5b95f43d3f74
diazraul2001 <91148063+diazraul2001@users.noreply.github.com>
2021-10-19 19:52:26 +0200

    a

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,11 @@
-// module.exports.list = (req, res) => {
-//     req.query
-//     };
-// module.exports.profile = (req, res) => {
-//         req.query
-//         };
-
 const users = [
     {id:1, name: 'jhon'},
     {id:2, name: 'david'}, 
     {id:3, name: 'maria'}
 ];
+
+const day = new Date();
+
 const status= {
     ok: 200,
     NotFound: 404,
@@ -20,7 +16,7 @@ const status= {
                 console.log(req.body);
                 res.status(200).send(users);
             },
-             profile: (req,res) => {
+            profile: (req,res) => {
                 //  console.log(req.params.userId);
                 const user = users.find((user)=> user.id == req.params.userId);
                 if(user){
@@ -29,6 +25,12 @@ const status= {
                     const msg = { error: "User Id not Found"};
                     res.status(status.NotFound).send(user);
                 }
-               
+
+            },
+            ej1: (req, res) => {
+                
+                res.status(200).send(day);
+                res.status(200).send(day.now());
+                // res.status(200).send(day.);
             }
         }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,4 +4,5 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 router.get('/list', userController.list);
 router.get('/profile/:userId', userController.profile);
+router.get('/EJ1', userController.ej1);
 module.exports = router;
