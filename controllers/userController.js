aa4f5ea8d253c1d09fc947d6b0002ee591a4b847
diazraul2001 <91148063+diazraul2001@users.noreply.github.com>
2021-10-19 19:15:39 +0200

    re

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,18 +6,29 @@
 //         };
 
 const users = [
-    {name: 'jhon'},
-    {name: 'david'}, 
-    {name: 'maria'}
+    {id:1, name: 'jhon'},
+    {id:2, name: 'david'}, 
+    {id:3, name: 'maria'}
 ];
 const status= {
-    ok:200,
+    ok: 200,
+    NotFound: 404,
+
 };
         module.exports = {
             list: (req, res) => {
-                res.status(200).send(users)
+                console.log(req.body);
+                res.status(200).send(users);
             },
-            // profile: () => {
-
-            // }
+             profile: (req,res) => {
+                //  console.log(req.params.userId);
+                const user = users.find((user)=> user.id == req.params.userId);
+                if(user){
+                    res.status(status.ok).send(user);
+                }else {
+                    const msg = { error: "User Id not Found"};
+                    res.status(status.NotFound).send(user);
+                }
+               
+            }
         }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const userController = require('../controller/userController');
-router.get('list', userController.list);
+const userController = require('../controllers/userController');
+router.get('/list', userController.list);
+router.get('/profile/:userId', userController.profile);
 module.exports = router;
